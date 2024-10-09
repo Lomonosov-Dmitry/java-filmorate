12911ea6123e509c65eceff1091cf1728e23d035
Lomonosov-Dmitry <cooler7@icloud.com>
2024-10-09 11:58:03 +0300

    Реализован функционал, описанный в ТЗ

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,64 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import ch.qos.logback.classic.Logger;
+import jakarta.validation.Valid;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+
+    private final Map<Integer, Film> films = new HashMap<>();
+    private static final Logger log = (Logger) LoggerFactory.getLogger(FilmController.class);
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        return films.values();
+    }
+
+    @PostMapping
+    public Film create(@Valid @RequestBody Film film) {
+        log.info("Добавляем новый фильм {}", film.getName());
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.error("Дата релиза должна быть позже 28.12.1895");
+            throw new ValidationException("Дата релиза должна быть позже 28.12.1895");
+        }
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @PutMapping
+    public Film update(@Valid @RequestBody Film film) {
+        log.info("Обновляем фильм {}", film.getName());
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.error("Дата релиза должна быть позже 28.12.1895");
+            throw new ValidationException("Дата релиза должна быть позже 28.12.1895");
+        }
+        if (!films.containsKey(film.getId())) {
+            log.error("Фильм с id = {} не найден", film.getId());
+            throw new NotFoundException("Такой фильм не найден");
+        }
+        films.replace(film.getId(), film);
+        return film;
+    }
+
+    private int getNextId() {
+        int currentMaxId = films.keySet()
+                .stream()
+                .mapToInt(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,64 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import ch.qos.logback.classic.Logger;
+import jakarta.validation.Valid;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final Map<Integer, User> users = new HashMap<>();
+    private static final Logger log = (Logger) LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@Valid @RequestBody User user) {
+        log.info("Создаем нового пользователя {}", user.getLogin());
+        if (user.getLogin().contains(" ")) {
+            log.error("Логин не должен содержать пробелы");
+            throw new ValidationException("Логин не должен содержать пробелы");
+        }
+        user.setId(getNextId());
+        if (user.getName() == null || user.getName().isBlank() || user.getName().isEmpty())
+            user.setName(user.getLogin());
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User update(@Valid @RequestBody User user) {
+        if (!users.containsKey(user.getId())) {
+            log.error("Пользователь не найден");
+            throw new NotFoundException("Пользователь не найден");
+        }
+        if (user.getLogin().contains(" ")) {
+            log.error("Логин не должен содержать пробелы");
+            throw new ValidationException("Логин не должен содержать пробелы");
+        }
+        log.info("Обновляем пользователя {}", user.getLogin());
+        users.replace(user.getId(), user);
+        return user;
+    }
+
+    private int getNextId() {
+        int currentMaxId = users.keySet()
+                .stream()
+                .mapToInt(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}

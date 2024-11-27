d1e32ef2bae706a5051852dbed7b66afec6baffd
Lomonosov-Dmitry <cooler7@icloud.com>
2024-11-27 10:39:03 +0300

    Реализовано взаимодействие с БД

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Ratings (Name)
-VALUES ('G'), ('PG'), ('PG-13'), ('R'), ('NC-17');
-
-INSERT INTO Genres (Name)
-VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');

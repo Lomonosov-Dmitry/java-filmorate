f1868afba157f7236ce1df571d9d73b820675d6f
Lomonosov-Dmitry <cooler7@icloud.com>
2024-11-27 09:37:04 +0300

    Реализовано взаимодействие с БД

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,29 +1,23 @@
-INSERT INTO Ratings (Id, Name)
-VALUES (1, 'G');
-INSERT INTO Ratings (Id, Name)
-VALUES (2, 'PG');
-INSERT INTO Ratings (Id, Name)
-VALUES (3, 'PG-13');
-INSERT INTO Ratings (Id, Name)
-VALUES (4, 'R');
-INSERT INTO Ratings (Id, Name)
-VALUES (5, 'NC-17');
+INSERT INTO Ratings (Name)
+VALUES ('G');
+INSERT INTO Ratings (Name)
+VALUES ('PG');
+INSERT INTO Ratings (Name)
+VALUES ('PG-13');
+INSERT INTO Ratings (Name)
+VALUES ('R');
+INSERT INTO Ratings (Name)
+VALUES ('NC-17');
 
-INSERT INTO Genres (Id, Name)
-VALUES (1, 'Комедия');
-INSERT INTO Genres (Id, Name)
-VALUES (2, 'Драма');
-INSERT INTO Genres (Id, Name)
-VALUES (3, 'Мультфильм');
-INSERT INTO Genres (Id, Name)
-VALUES (4, 'Триллер');
-INSERT INTO Genres (Id, Name)
-VALUES (5, 'Документальный');
-INSERT INTO Genres (Id, Name)
-VALUES (6, 'Боевик');
-
---INSERT INTO Films (Id, Name, Description, ReleaseDate, Duration)
---VALUES (0, 'asd', 'asdfasdf', '2022-02-02', 120);
-
---INSERT INTO Users (Id, Email, Login, Name, Birthday)
---VALUES (0, 'aaa@bbb.com', 'asdfasdf', 'Dfg' ,'1982-02-02');
+INSERT INTO Genres (Name)
+VALUES ('Комедия');
+INSERT INTO Genres (Name)
+VALUES ('Драма');
+INSERT INTO Genres (Name)
+VALUES ('Мультфильм');
+INSERT INTO Genres (Name)
+VALUES ('Триллер');
+INSERT INTO Genres (Name)
+VALUES ('Документальный');
+INSERT INTO Genres (Name)
+VALUES ('Боевик');
